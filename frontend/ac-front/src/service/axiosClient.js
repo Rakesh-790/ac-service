4cbd1e11de84a1b393926a086d975f752c689853
Rakesh_790 <brakeshroshan098@gmail.com>
2026-02-20 12:01:59 +0530

    refactor axiosClient to remove local storage token handling and streamline request configuration

--- frontend/ac-front/src/service/axiosClient.js
+++ frontend/ac-front/src/service/axiosClient.js
@@ -1,38 +1,22 @@
 import axios from "axios";
 
-let accessToken = localStorage.getItem("accessToken") || null;
-
-export function setAccessToken(token) {
-  accessToken = token;
-}
 
 // Create instance
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1200"
-});
-
-// Attach token
-axiosClient.interceptors.request.use((config) => {
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
+  baseURL: "http://localhost:1200",
+  withCredentials: true,
 });
 
 // Refresh token function
 async function refreshAccessToken() {
-  const refreshToken = localStorage.getItem("refreshToken");
-  if (!refreshToken) throw new Error("No refresh token");
 
   const { data } = await axios.post(
     "http://localhost:1200/auth/refresh",
-    { refreshToken }
+    {},
+    {
+      withCredentials: true,
+    }
   );
-
-  accessToken = data.accessToken;
-  localStorage.setItem("accessToken", data.accessToken);
-
-  return data.accessToken;
 }
 
 // Response interceptor
@@ -55,11 +39,9 @@ axiosClient.interceptors.response.use(
       originalRequest._retry = true;
 
       try {
-        const newToken = await refreshAccessToken();
-        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        await refreshAccessToken();
         return axiosClient(originalRequest);
       } catch (err) {
-        console.log("Session expired. Please login again.");
         return Promise.reject(err);
       }
     }
